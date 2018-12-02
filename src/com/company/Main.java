7fe42f327fc54ee926020ed1688858b6c19a7424
jagra26 <31116911+jagra26@users.noreply.github.com>
2018-12-02 14:57:13 -0300

    main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System labS = new System();
+	boolean exit = false;
+	while (!exit){
+	    exit = labS.display(labS);
+    }
     }
 }
