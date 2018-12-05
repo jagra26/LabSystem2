f4043f9a8fdd12e9c9a35cef26db1ec1bb73d8ed
jagra26 <31116911+jagra26@users.noreply.github.com>
2018-12-04 21:16:12 -0300

    guidance

--- src/com/company/Guidance.java
+++ src/com/company/Guidance.java
@@ -1,7 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Guidance {
     Teacher masterMind;
     int[][] students;
+    public Guidance(Teacher masterMind){
+        this.students = new int[3][1000];
+        this.masterMind = masterMind;
+        Scanner input =  new Scanner(java.lang.System.in);
+        int cases;
+        int login;
+        java.lang.System.out.print("add your students\n PhD - 1 Mastering - 2 Undergraduate -3 exit - 4\n");
+        cases = input.nextInt();
+        java.lang.System.out.print("insert the login\n");
+        login = input.nextInt();
+        while (true){
+            java.lang.System.out.print("add your students\n PhD - 1 Mastering - 2 Undergraduate -3 exit - 4\n");
+            cases = input.nextInt();
+            if (cases == 4)
+            {
+                break;
+            }
+            java.lang.System.out.print("insert the login\n");
+            login = input.nextInt();
+            this.students[cases-1][login] = 1;
+        }
+    }
 
 }
